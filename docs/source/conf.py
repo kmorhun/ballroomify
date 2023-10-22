0675c9e5aa975676b1847ff36c3938faef713df5
kmorhun <kmorhun@gmail.com>
2023-10-22 00:55:28 -0400

    tried to add modules to path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,15 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
 project = 'ballroomify'
 copyright = '2023, Kateryna Morhun'
 author = 'Kateryna Morhun'
 release = '0.0.0'
+
+sys.path.insert(0, os.path.abspath('../../src'))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
